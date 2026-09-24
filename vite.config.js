461013520de233fe27698f61f60fcf4f2225300d
pophealth-odc <monjanuary21@gmail.com>
2026-09-24 16:43:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/conman/",
+export default defineConfig({  
   plugins: [react(), tailwindcss()],
+  base: "/conman/",
 })
