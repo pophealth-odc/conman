30b22e12ff11ed4b397c7a6ea3ef44b5972c2d82
pophealth-odc <monjanuary21@gmail.com>
2026-09-24 16:42:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pophealth-odc/conman/",
+  base: "/conman/",
   plugins: [react(), tailwindcss()],
 })
